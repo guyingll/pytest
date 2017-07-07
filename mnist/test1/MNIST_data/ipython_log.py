4cb4c9b17e501b07291effd040148b05b2ae8216
unknown <yanpeng@zyjr.com>
2017-07-07 18:53:11 +0800

    init

---
+++ mnist/test1/MNIST_data/ipython_log.py
@@ -0,0 +1,16 @@
+# IPython log file
+
+get_ipython().magic(u'logstart')
+get_ipython().system(u'date')
+a=2+2
+a
+get_ipython().magic(u'hits')
+get_ipython().magic(u'hist')
+get_ipython().magic(u'hist -g a=2')
+help
+help ar
+help arange
+help arange
+arange
+get_ipython().magic(u'pinfo arange')
+exit()
---
+++ mnist/test1/test.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plot
+import numpy as nuy
+
+if __name__=="__main__":
+    a=npy.arange(7)
+    print (a)
+    b=npy.sinc(a)
+    print (b)
+    plot(b)
